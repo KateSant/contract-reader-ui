9ffc8431f3564690bc93a9918d3775cceb3a5a69
Kate Sant <kate.sant@yahoo.com>
2021-05-13 16:05:56 +0100

    Basic file upload and read

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 # Build stage
 FROM maven AS build
+WORKDIR /workspace
 COPY pom.xml .
 COPY src src
-RUN mvn -f pom.xml clean package
+RUN mvn -f pom.xml clean compile test package
 
 # Image for deploy
 FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-COPY target/*.jar app.jar
+COPY --from=build /workspace/target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/thinktalkbuild/contractreader/controller/UploadController.java
+++ src/main/java/com/thinktalkbuild/contractreader/controller/UploadController.java
@@ -1,11 +1,17 @@
 package com.thinktalkbuild.contractreader.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -14,13 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class UploadController {
 
-        
-        @GetMapping("/")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
-        
-        
+    @GetMapping("/")
+    public String greeting(Model model) {
+        return "upload";
+    }
 
+    @PostMapping("/upload-file")
+    public String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+
+        if (file.isEmpty()) {
+            model.addAttribute("errormessage", "File was empty.");
+            return "analysis";
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+            
+            String firstLine = reader.readLine();
+            reader.close();
+            
+            model.addAttribute("contents", firstLine);
+
+        } catch (Exception ex) {
+            model.addAttribute("errormessage", "An error occurred processing the file.");
+  
+        }
+        return "analysis";
+
+    }
 }
