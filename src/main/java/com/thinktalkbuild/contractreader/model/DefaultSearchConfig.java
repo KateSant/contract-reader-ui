1d599502421dddabeac70120a17cd9081b34e6c3
Kate Sant <kate.sant@yahoo.com>
2021-05-19 21:16:06 +0100

    templated the head element

--- src/main/java/com/thinktalkbuild/contractreader/model/DefaultSearchConfig.java
+++ src/main/java/com/thinktalkbuild/contractreader/model/DefaultSearchConfig.java
@@ -1,12 +1,14 @@
 package com.thinktalkbuild.contractreader.model;
 
-import com.thinktalkbuild.contractreader.model.SearchCriteria;
+
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class DefaultSearchConfig implements SearchConfig{
 
     private List<SearchCriteria> searchCriteria = new ArrayList<>();
