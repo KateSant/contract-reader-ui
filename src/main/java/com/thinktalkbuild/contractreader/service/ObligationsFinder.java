ca38e73f9b1d093df8d3afd30a958fbb960cb1a2
Kate Sant <kate.sant@yahoo.com>
2021-05-20 16:43:33 +0100

    Regex for finding obligations

--- src/main/java/com/thinktalkbuild/contractreader/service/ObligationsFinder.java
+++ src/main/java/com/thinktalkbuild/contractreader/service/ObligationsFinder.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -18,11 +20,44 @@ import java.util.stream.Collectors;
 @Service
 public class ObligationsFinder {
 
+    public static List<String> OBLIGING_VERBS = new ArrayList<>(List.of("shall", "must"));
+    public static String regex = ".*?" // The beginning of the sentence (zero or more any character, non greedy match)
+                                + "([^\\s]+)" // The party.   A word.  (Any character other than space, 1 or more times.)
+                                + " " // A space
+                                + "(%s)" // must or shall (inserted later)
+                                + " " // A space
+                                +".+?" // The end of the sentence (zero or more any character, non-greedy match)
+                                +"\\."; // A full stop
+
+
 
     protected List<Obligation> findObligations(List<String> inputParagraphs) {
         List<Obligation> obligations = new ArrayList<>();
+        inputParagraphs.stream().forEach(para -> {
+            obligations.addAll(findObligations(para));
+        });
         return obligations;
     }
+
+    protected List<Obligation> findObligations(String inputParagraph) {
+        List<Obligation> obligations = new ArrayList<>();
+        String regexWithVerb = String.format(regex, "must");
+        Pattern pattern = Pattern.compile(regexWithVerb, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputParagraph);
+        while (matcher.find()) {
+            System.out.println("0="+ matcher.group(0)+" 1="+matcher.group(1)+" 2="+matcher.group(2));
+            Obligation obl = new Obligation();
+            obl.setWholeSentence(matcher.group(0));
+            obl.setParty(matcher.group(1));
+            obl.setObligingVerb(matcher.group(2));
+            obligations.add(obl);
+        }
+
+
+
+        return obligations;
+    }
+
 
 
 }
--- src/test/java/com/thinktalkbuild/contractreader/service/ObligationsFinderTests.java
+++ src/test/java/com/thinktalkbuild/contractreader/service/ObligationsFinderTests.java
@@ -12,8 +12,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
@@ -21,21 +20,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class ObligationsFinderTests {
 
+    private ObligationsFinder obligationsFinder= new ObligationsFinder();
 
-    private ObligationsFinder obligationsFinder;
+    static String DUMMY_PARAGRAPH = "This is the first sentence, it says that The Supplier must sit down.  This is the second sentence, it says that a Supplier must also stand up.  " +
+            "This is the third sentence in the paragraph, it says that The Customer shall do a hand stand.";
 
-    @BeforeEach
-    void setup(){ obligationsFinder= new ObligationsFinder();
-    }
-
-    static List<String> DUMMY_INPUT_DATA = new ArrayList<>(List.of(
-            "This is the first paragraph, it says that The Supplier must sit down.",
+    static List<String> DUMMY_PARAGRAPHS_LIST = new ArrayList<>(List.of(
+            DUMMY_PARAGRAPH,
             "This is the second paragraph, it says that The Customer shall stand up."));
 
     @Test
-    void givenMust_findsObligationSentence() {
-        List<Obligation> output = obligationsFinder.findObligations(DUMMY_INPUT_DATA);
-        assertTrue(output.get(0).getWholeSentence().contains("sit down."));
+    void testFindObligations_finds_must() {
+
+        List<Obligation> obligations =  obligationsFinder.findObligations(DUMMY_PARAGRAPH);
+        assertTrue(obligations.size()>0);
+        Obligation firstSentence = obligations.get(0);
+        assertEquals(firstSentence.getWholeSentence(), "This is the first sentence, it says that The Supplier must sit down.");
+        assertEquals(firstSentence.getObligingVerb(), "must");
+        assertEquals(firstSentence.getParty(), "Supplier");
+    }
+
+
+
+    @Test
+    void testFindsObligationsInParagraphs() {
+        //List<Obligation> output = obligationsFinder.findObligations(DUMMY_PARAGRAPHS_LIST);
+        //assertTrue(output.get(0).getWholeSentence().contains("sit down."));
     }
 
 
