771ff7bd0c49064547c3bee566476dd41a7afbf0
Kate Sant <kate.sant@yahoo.com>
2023-02-06 23:07:45 +0000

    CR-20 Add nav burger and js to support it.

--- src/main/resources/static/js/controls.js
+++ src/main/resources/static/js/controls.js
@@ -1,3 +1,16 @@
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
+
+
 const fileInput = document.querySelector('#file-js input[type=file]');
             fileInput.onchange = () => {
                 if (fileInput.files.length > 0) {
@@ -20,27 +33,3 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " is-active";
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-
-   alert('dom loaded');
-
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Add a click event on each of them
-  $navbarBurgers.forEach( el => {
-    el.addEventListener('click', () => {
-
-      // Get the target from the "data-target" attribute
-      const target = el.dataset.target;
-      const $target = document.getElementById(target);
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle('is-active');
-      $target.classList.toggle('is-active');
-
-    });
-  });
-
-});
